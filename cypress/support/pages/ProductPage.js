1997e1682a97755a2f0085da9eb45b300d6e1cc1
enginSacan <engnsacan@gmail.com>
2024-07-12 11:49:27 +0200

    Correcting expected result for product header

--- cypress/support/pages/ProductPage.js
+++ cypress/support/pages/ProductPage.js
@@ -1,9 +1,14 @@
+import { expect } from "chai";
+
 export class ProductPage {
 
     checkAllProductHeaders (product) {
         cy.get(".a-size-base-plus.a-color-base.a-text-normal").each(
             (item,index,list) => {
-                cy.wrap(item).should('contain.text',product)
+                cy.wrap(item).invoke('text').then( (str) => {
+                    const lowerCaseString = str.toLowerCase();
+                    expect(lowerCaseString).to.contains(product);
+                })            
             }
         )
     }
